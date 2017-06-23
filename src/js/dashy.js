c698554623200e003530059f84ce742b840d8a03
Federico Rossi <f.rossi57@studenti.unipi.it>
2017-06-23 20:24:35 +0200

    So smooth

--- src/js/dashy.js
+++ src/js/dashy.js
@@ -1,21 +1,50 @@
+var Dashy ={
+};
+
 function showMail(obj) {
 	var mail = "federico.rossi15s95@gmail.com"
 	$(obj).html(mail);
 	$(obj).attr("href","mailto:"+mail);
 }
-var threshold = $('.dashy-topbar').height();
-var addPad = $('.dashy-topbar').outerHeight();
-$(window).bind('scroll', function () {
-    if ($(window).scrollTop() > threshold) {
-        $('.dashy-topbar').addClass('fixed');
-        $('body').css({'padding-top':addPad+"px"})
-    } else {
-        $('.dashy-topbar').removeClass('fixed');
-        $('body').css({'padding-top':0})        
+
+function init() {
+    Dashy.permittedLocations = ["about","projects","contacts"];
+    Dashy.topBarInnerHeight = $('.dashy-topbar').height();
+    Dashy.topBarOuterHeight = $('.dashy-topbar').outerHeight();
+    Dashy.responsiveTriggerWidth = 768;
+    $("#top").animate({'padding-top':Dashy.topBarOuterHeight+"px"},250,'swing',function() {
+        var hashLocation = ($.inArray(window.location.hash,Dashy.permittedLocations)>-1)? window.location.hash:'about';
+        scrollTo($("a[href$='"+hashLocation+"']"),true);
+    });
+    $("body").scrollspy({
+        target:"#topbar",
+        offset:Dashy.topBarOuterHeight
+    })
+    $("#topbar li").on("activate.bs.scrollspy",function(el) {
+        highlightCurrentAnchor(el.target);
+    });
+}
+
+function highlightCurrentAnchor(sectionAnchor) {
+    if(Dashy.lastVisitedAnchor) {
+        $(Dashy.lastVisitedAnchor).parent().removeClass("dashy-active");
     }
-});
+    $(sectionAnchor).parent().addClass("dashy-active");
+    Dashy.lastVisitedAnchor = sectionAnchor;    
+}
 
+function scrollTo(sectionAnchor,forced) {
+    var sectionId = $(sectionAnchor).attr("href");
+    var section = $(sectionId);
+    var sectionTop = Math.round(section.position().top);
+    var sectionTopOffset = sectionTop - Dashy.topBarOuterHeight;
+    $("body").animate({'scrollTop':sectionTopOffset},500,'swing',function() {
+        if(!forced && $(window).width() <= Dashy.responsiveTriggerWidth)
+            $("#dashy-topbar-toggle").click();
+    });
+    highlightCurrentAnchor(sectionAnchor);
+}
 
-function toTop() {
-	$(window).scrollTop(0);
+function scrollToTop() {
+    $("body").animate({'scrollTop':0},250,'swing');
 }
